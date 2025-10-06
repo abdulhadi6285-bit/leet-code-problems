de3a42a4a7591601406ddef80d2cf5f976b77693
HADI KALEEM <hadikaleem@Mac.localdomain>
2025-10-06 16:17:24 +0500

    Initial commit. Completed problem#1

---
+++ easy/code.py
@@ -0,0 +1,16 @@
+# Problem #1
+
+# # 
+# def twoSum(nums: List[int], target: int) -> List[int]:
+#     seen = {}
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in seen:
+#             return [seen[complement], i]
+#         seen[num] = i
+
+def sum(num1, num2):
+  return num1 + num2
+
+print(sum(1, 3))
+
