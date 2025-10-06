ca8d73fc0d89bee05a3ab783562ba3b802b5b9ee
HADI KALEEM <hadikaleem@Mac.localdomain>
2025-10-06 16:27:48 +0500

    Added problem #2. isPalindrom.

--- easy/code.py
+++ easy/code.py
@@ -9,8 +9,21 @@
 #             return [seen[complement], i]
 #         seen[num] = i
 
+print("Problem #1 - sum")
 def sum(num1, num2):
   return num1 + num2
 
-print(sum(1, 3))
+print("sum(1, 3) is: ",sum(1, 3))
+print("--------------------")
 
+def isPalindrome(x: int) -> bool:
+
+    if x < 0:
+        return False
+
+    s = str(x)
+    return s == s[::-1]
+
+print("Problem #2 - isPalindrom?")
+print("isPalindrome(121)? ", isPalindrome(121))
+print("isPalindrome(223)? ", isPalindrome(223))
