efa6dd95e38aa86a818691db6f174656adc97b4d
HADI KALEEM <hadikaleem@Mac.localdomain>
2025-10-08 12:06:57 +0500

    Added problem 9. Search Insert Position

--- easy/code.py
+++ easy/code.py
@@ -164,3 +164,27 @@ print("strStr('sadbutsad', 'sad')? ", strStr("sadbutsad", "sad"))
 print("strStr('leetcode', 'leeto')? ", strStr("leetcode", "leeto"))
 print("strStr('hello', 'll')? ", strStr("hello", "ll"))
 print("strStr('aaaaa', 'bba')? ", strStr("aaaaa", "bba"))
+
+
+from typing import List
+
+def searchInsert(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+
+print("Problem #9 - Search Insert Position")
+print("searchInsert([1,3,5,6], 5)? ", searchInsert([1,3,5,6], 5))
+print("searchInsert([1,3,5,6], 2)? ", searchInsert([1,3,5,6], 2))
+print("searchInsert([1,3,5,6], 7)? ", searchInsert([1,3,5,6], 7))
+print("searchInsert([1,3,5,6], 0)? ", searchInsert([1,3,5,6], 0))
