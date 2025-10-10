b93d664bdd6d2ba649512deaf2f1be84bfd62a51
HADI KALEEM <hadikaleem@Mac.localdomain>
2025-10-10 11:05:21 +0500

    Added problem 3. Longest Palindrome

--- medium/code.py
+++ medium/code.py
@@ -55,3 +55,35 @@ print("lengthOfLongestSubstring('abcabcbb')? ", lengthOfLongestSubstring("abcabc
 print("lengthOfLongestSubstring('bbbbb')? ", lengthOfLongestSubstring("bbbbb"))
 print("lengthOfLongestSubstring('pwwkew')? ", lengthOfLongestSubstring("pwwkew"))
 print("lengthOfLongestSubstring('dvdf')? ", lengthOfLongestSubstring("dvdf"))
+
+
+def longestPalindrome(s: str) -> str:
+    if len(s) <= 1:
+        return s
+
+    start, end = 0, 0
+
+    def expand_from_center(left: int, right: int) -> tuple:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+
+    for i in range(len(s)):
+        l1, r1 = expand_from_center(i, i)
+        l2, r2 = expand_from_center(i, i + 1)
+
+        if r1 - l1 > end - start:
+            start, end = l1, r1
+        if r2 - l2 > end - start:
+            start, end = l2, r2
+
+    return s[start:end+1]
+
+
+print("Problem #3 - longestPalindrome")
+print("longestPalindrome('babad')? ", longestPalindrome("babad"))
+print("longestPalindrome('cbbd')? ", longestPalindrome("cbbd"))
+print("longestPalindrome('a')? ", longestPalindrome("a"))
+print("longestPalindrome('ac')? ", longestPalindrome("ac"))
+print("longestPalindrome('racecar')? ", longestPalindrome("racecar"))
